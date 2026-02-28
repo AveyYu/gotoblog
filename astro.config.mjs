89b1cb6d2aca240f53d5de7b277ea6c2cb91fd43
humbornjo <humbornjo@gmail.com>
2026-03-01 02:35:28 +0800

    chore: use domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://humbornjo.github.io',
-  base: '/nobloger',
+  site: "https://humbornjo.github.io",
+  base: "/",
   prefetch: {
-    prefetchAll: true
-  }
+    prefetchAll: true,
+  },
 });
