e4e158ac886c11410be52b8b776202e9fd1c2a96
AveyYu <50583242+AveyYu@users.noreply.github.com>
2026-05-28 16:57:46 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'noblog',
-    description: 'bloger cipher',
+    title: 'OKGOWILD',
+    description: 'okgowild',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./posts/*.{md,mdx}')),
     customData: `<language>en-us</language>`,
